79895956804ffbbd9349ea95f7bc94c33292ee72
Lekhanapriya-295 <karakavalasalekhanapriya133@gmail.com>
2026-01-28 17:55:08 +0530

    Data Type

---
+++ DataType.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class DataTypes{
+       public static void main(String[] args){
+            int A=10;
+            boolean bl=true;
+            short s=20;
+            byte b=1;
+            char c='A';
+            System.out.println(A);
+            System.out.println(bl);
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(s);
+}
+}
