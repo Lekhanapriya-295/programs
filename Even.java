2b25988ce3cdd21e7b89819c65694b9595b0aae0
Lekhanapriya-295 <karakavalasalekhanapriya133@gmail.com>
2026-01-30 20:48:54 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class EvenOdd{
+     public static void main(String args[]){
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter any number:");
+     int n=sc.nextInt();
+     if(n%2==0)
+{
+System.out.println("Even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
+
+     
