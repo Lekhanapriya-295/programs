0a699266d4510367dc617706b538c5f76ed9efee
Lekhanapriya-295 <karakavalasalekhanapriya133@gmail.com>
2026-02-05 17:05:03 +0530

    Add files via upload

---
+++ Welcomeuser.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Welcomeuser{
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter your name:");
+    String name=sc.nextLine();
+    System.out.println("welcome"+""+name);
+}
+}
