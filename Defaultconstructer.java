650452068c9b7d8f50624ec28cbd0a48ec53e28d
Lekhanapriya-295 <karakavalasalekhanapriya133@gmail.com>
2026-02-07 16:48:31 +0530

    Add files via upload

---
+++ Defaultconstructer.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Defaultconstructer
+{
+int age;
+String name;
+Defaultconstructer()
+{
+age=19;
+name="lekhana";
+}
+public static void main(String args[])
+{
+Defaultconstructer obj=new Defaultconstructer();
+System.out.println("name is:"+obj.name);
+System.out.println("age is:"+obj.age);
+}
+}
