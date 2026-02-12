8813aa78a42c4143ff919df4f95d48f367f1de5a
Lekhanapriya-295 <karakavalasalekhanapriya133@gmail.com>
2026-02-12 21:25:19 +0530

    Add files via upload

---
+++ StringMethods.java
@@ -0,0 +1,12 @@
+class StringMethods{
+public static void main(String args[])
+{
+String str="Lekhana ";
+System.out.println("Upper case:"+str.toUpperCase());
+System.out.println("Lower case:"+str.toLowerCase());
+System.out.println("Length:"+str.length());
+System.out.println("concatenation:"+str.concat("priya"));
+System.out.println("position:"+str.charAt(0));
+System.out.println("Index:"+str.indexOf(2));
+}
+}
