8839d5b0af4101694f95261f83285fd5396b3d10
Lekhanapriya-295 <karakavalasalekhanapriya133@gmail.com>
2026-02-15 18:06:08 +0530

    Add files via upload

---
+++ Instancevariable.java
@@ -0,0 +1,9 @@
+class Instancevariable
+{
+int a=95;
+public static void main(String args[])
+{
+Instancevariable obj=new Instancevariable();
+System.out.println("Instance variable is:"+ obj.a);
+}
+}
