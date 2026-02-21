f1a50a4201142121e8787a22c068daa8f6cb7dfc
Lekhanapriya-295 <karakavalasalekhanapriya133@gmail.com>
2026-02-21 16:51:24 +0530

    Add files via upload

---
+++ oneDarray.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class oneDarray{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a[]=new int[5];
+System.out.println("enter array elements:");
+for(int i=0;i<a.length;i++)
+{
+a[i]=sc.nextInt();
+}
+System.out.println("Array elements are:");
+for(int i=0;i<a.length;i++)
+{
+System.out.print(a[i]);
+}
+}
+}
+
