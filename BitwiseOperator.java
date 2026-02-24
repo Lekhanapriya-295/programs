5c361eedeb062e8e046856b130b29d77a95f2dca
Lekhanapriya-295 <karakavalasalekhanapriya133@gmail.com>
2026-02-24 17:41:36 +0530

    Add files via upload

---
+++ BitwiseOperator.java
@@ -0,0 +1,7 @@
+public class BitwiseOperator { 
+    public static void main(String[] args) { 
+      int x = 5, y = 3; 
+        System.out.println(x & y); // 1 (Bitwise AND) 
+        System.out.println(x | y); // 7 (Bitwise OR) 
+    } 
+}
