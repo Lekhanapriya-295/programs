47e64d375a76f1fa98364d519da0ca255be0e5ee
Lekhanapriya-295 <karakavalasalekhanapriya133@gmail.com>
2026-03-04 21:17:01 +0530

    Add files via upload

---
+++ LogicalOperatorExample.java
@@ -0,0 +1,10 @@
+public class LogicalOperatorExample
+{
+  public static void main(String args[])
+{
+ boolean X=true,Y=false;
+ System.out.println(X && Y);
+ System.out.println(X || Y);
+ System.out.println(!X);
+}
+}
