c2f50aa08475412a3518543195b3cee5bd5f09a9
Lekhanapriya-295 <karakavalasalekhanapriya133@gmail.com>
2026-03-05 23:14:42 +0530

    Add files via upload

---
+++ ArithmeticOperator.java
@@ -0,0 +1,11 @@
+public class ArithmeticOperator
+{
+public static void main(String args[])
+{
+  int a=10,b=2;
+  int sum=a+b;
+  int product=a*b;
+  System.out.println("sum:"+sum);
+  System.out.println("product:"+product);
+}
+}
