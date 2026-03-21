d14227d4ecfca6d9fc430dff9215c3c182f3f1e8
Lekhanapriya-295 <karakavalasalekhanapriya133@gmail.com>
2026-03-21 21:26:15 +0530

    Add files via upload

---
+++ FileNotFoundExceptionExample.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+class  FileNotFoundExceptionExample{
+    public static void main(String[] args) throws FileNotFoundException {
+        FileInputStream file = new FileInputStream("abc.txt");
+    }
+}
